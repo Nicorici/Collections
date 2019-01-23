7a8c4067df14b731bff430f2c7f48194f621a98b
Nicorici <nickorici@yahoo.com>
2019-01-23 16:48:00 +0200

    The List property was added to the Node Class

--- Collections/Node.cs
+++ Collections/Node.cs
@@ -6,6 +6,7 @@ namespace Collections
 {
     public class Node<T>
     {
+        public LinkedList<T> List { get; set; }
         public Node<T> Next { get;  set; }
         public Node<T> Previous { get;set; }
         public T Value { get; set;}
